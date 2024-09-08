7f6f20f41b1c05b3b24f9cc47ecb2e9e15b34739
CodeExplode <79982924+CodeExplode@users.noreply.github.com>
2024-09-08 16:28:41 +1000

    Add files via upload

--- conditioner_network.py
+++ conditioner_network.py
@@ -7,14 +7,21 @@ class ConditioningAdjustmentNetwork(nn.Module):
         super(ConditioningAdjustmentNetwork, self).__init__()
         
         self.fc1 = nn.Linear(input_dim, hidden_dim)
+        #self.relu = nn.ReLU() # don't think this is ideal, as conditioning vectors do have negative values
         self.fc2 = nn.Linear(hidden_dim, input_dim)
-        self.relu = nn.ReLU()
+        
+        # attempt to have the data pass through unchanged in the default version of the network, initializing weights as an identity matrix and zero biases
+        nn.init.eye_(self.fc1.weight)
+        nn.init.zeros_(self.fc1.bias)
+        
+        nn.init.eye_(self.fc2.weight)
+        nn.init.zeros_(self.fc2.bias)
     
     # conditioning: (batch_size, 77, 768) input conditioning vectors
     # mask: (batch_size, 77) marking the positions of BOS and EOS vectors to be altered
     def forward(self, conditioning, mask):
         adjusted_conditioning = self.fc1(conditioning)
-        adjusted_conditioning = self.relu(adjusted_conditioning)
+        #adjusted_conditioning = self.relu(adjusted_conditioning)
         adjusted_conditioning = self.fc2(adjusted_conditioning)
         
         mask = mask.unsqueeze(-1) # (batch_size, 77, 1)
